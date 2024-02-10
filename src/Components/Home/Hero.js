af30caa732dc1c820d199c94cca9b9c59996d026
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-10 17:46:43 -0600

    Hero has most CSS completed, APP CSS has been removed from template style and Hero JS has been updated

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -13,29 +13,32 @@ function Hero() {
     <div className='Hero'>
       <div className="hero__container">
         <div className="hero__text__container">
-            <h1>A STYLE FOR EVERY OCCASION</h1>
-            <h4>Lingerie, Sleepwear, and more... Curated just for <strong>YOU!</strong></h4>
-            <button>Take Style Quiz</button>
-            <div className="sizing__container">
-                
-                {/* NAME AND SIZE  */}
-                <div className="name__and__size">
-                    <h4>BANDS</h4>
-                    <h4>32-48</h4>
-                </div>
-                
-                {/* NAME AND SIZE  */}
-                <div className="name__and__size">
-                    <h4>WAISTS</h4>
-                    <h4>24-40</h4>
-                </div>
+            <div className="hero__text">
+                <h1>A STYLE FOR EVERY OCCASION</h1>
+                <h4>Lingerie, Sleepwear, and more... Curated just for <strong>YOU!</strong></h4>
+                <button>Take Style Quiz</button>
+                <div className="sizing__container">
+                    
+                    {/* NAME AND SIZE  */}
+                    <div className="name__and__size">
+                        <h4>BANDS</h4>
+                        <h4>32-48</h4>
+                    </div>
+                    
+                    {/* NAME AND SIZE  */}
+                    <div className="name__and__size">
+                        <h4>WAISTS</h4>
+                        <h4>24-40</h4>
+                    </div>
 
-                {/* NAME AND SIZE  */}
-                <div className="name__and__size">
-                    <h4>SIZES</h4>
-                    <h4>4-8</h4>
+                    {/* NAME AND SIZE  */}
+                    <div className="name__and__size">
+                        <h4>SIZES</h4>
+                        <h4>4-8</h4>
+                    </div>
                 </div>
             </div>
+
         </div>
       </div>
 
