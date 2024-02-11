5e881489a23bbc7e6a4e310b2b6deabfea7b900e
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-10 19:32:31 -0600

    added product code to the project.

--- src/Components/Home/Box.js
+++ src/Components/Home/Box.js
@@ -3,7 +3,36 @@ import React from 'react'
 function Box() {
   return (
     <div className='BOX'>
-      
+      <h1>WHAT'S IN THE SEDUCTIVEBOX?</h1>
+      <p>Styles hand picked just for you, based on your unique preferences!</p>
+
+      <div className="box__container">
+        {/* INDIVIDUAL ITEM  */}
+        <div className="item__container">
+          <img src='images/box/.png' alt='product-img' />
+        </div>
+        
+        {/* INDIVIDUAL ITEM  */}
+        <div className="item__container">
+          <img src='images/box/.png' alt='product-img' />
+        </div>
+        
+        {/* INDIVIDUAL ITEM  */}
+        <div className="item__container">
+          <img src='images/box/.png' alt='product-img' />
+        </div>
+        
+        {/* INDIVIDUAL ITEM  */}
+        <div className="item__container">
+          <img src='images/box/.png' alt='product-img' />
+        </div>
+        
+        {/* INDIVIDUAL ITEM  */}
+        <div className="item__container">
+          <img src='images/box/.png' alt='product-img' />
+        </div>
+        
+      </div>
     </div>
   )
 }
