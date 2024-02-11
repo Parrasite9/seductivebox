579aae39c04ea470fec0e744221491d7d4393c65
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-10 19:14:58 -0600

    Restructured how Featured is nested

--- src/Components/Home/Featured.js
+++ src/Components/Home/Featured.js
@@ -3,12 +3,12 @@ import '../../CSS/Home/Featured.css'
 function Featured() {
   return (
     <div className='Featured'>
-      <div className="featured__container">
-        <div className="featured__text__container">
-            <div className="featured__text">
-                <h2>AS FEATURED IN</h2>
-            </div>
+        <div className="featured__header">
+            <h2>AS FEATURED IN</h2>
         </div>
+        
+      <div className="featured__container">
+
       </div>
     </div>
   )
