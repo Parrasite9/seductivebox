3d74685e1670c050198d26e3fa63bb757fccdfad
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-10 19:25:14 -0600

    Featured section completed for mobile

--- src/Components/Home/Featured.js
+++ src/Components/Home/Featured.js
@@ -6,8 +6,27 @@ function Featured() {
         <div className="featured__header">
             <h2>AS FEATURED IN</h2>
         </div>
-        
+
       <div className="featured__container">
+        {/* FEATURED IMAGE  */}
+        <div className="featured__img">
+          <img src="images/featured/yahoo.png" alt="yahoo" />
+        </div>
+
+        {/* FEATURED IMAGE  */}
+        <div className="featured__img">
+          <img src="images/featured/entrep.png" alt="entrepreneur" />
+        </div>
+
+        {/* FEATURED IMAGE  */}
+        <div className="featured__img">
+          <img src="images/featured/forbes.png" alt="forbes" />
+        </div>
+
+        {/* FEATURED IMAGE  */}
+        <div className="featured__img">
+          <img src="images/featured/nbc.png" alt="nbc" />
+        </div>
 
       </div>
     </div>
