e8cdb5c8ccb564aada36a2586b62574600d60fb1
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-10 19:28:19 -0600

    added a div in the Anchor. Will return to this file later.

--- src/Components/Home/Anchor.js
+++ src/Components/Home/Anchor.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Anchor() {
   return (
     <div className='Anchor'>
-       
+       <div className="anchor__container">
+        
+       </div>
     </div>
   )
 }
