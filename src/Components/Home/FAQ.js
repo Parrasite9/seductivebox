1c168fd5037e6b83b1aad5d6a2f0918cc542ca1f
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-11 21:21:10 -0600

    Added conditional to render more faqs at small and larger size screens to be fitting

--- src/Components/Home/FAQ.js
+++ src/Components/Home/FAQ.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import '../../CSS/Home/FAQ.css'
+import WindowWidth from '../CustomHook/WindowWidth'
 function FAQ() {
+
+  const width = WindowWidth()
+  const isSmallDesktop = width >= 1000 && width < 1250
+  
   return (
     <div className='Faq'>
       <div className="faq__header">
@@ -11,12 +16,48 @@ function FAQ() {
       <div className="faq__img">
         <img src="images/faq/faq.jpg" alt="model" />
       </div>
-      
-      <div className="faq__questions__container">
+
+      {isSmallDesktop ? (
+        <div className="faq__questions__container">
+            
+              {/* Q and A */}
+              <div className="q__and__a">
+                <h3>What if I don't like what I received?</h3>
+                <p>At SeductiveBox, our customers have ZERO RISK on their subscriptions! You don't like what you received? Return it!</p>
+              </div>
+              
+              {/* Q and A */}
+              <div className="q__and__a">
+                <h3>Do you offer plus size lingerie?</h3>
+                <p>Yes, we do! We proudly offer sizes across the entire spectrum of body types, including flattering plus size lingerie options in sizes 16-18.</p>
+              </div>
+              
+              {/* Q and A */}
+              <div className="q__and__a">
+                <h3>How much does it cost?</h3>
+                <p>Your SeductiveBox varies on the package you select, but can be as low as 29.99! Of course, you will always have the option to skip a month’s delivery, and you can cancel at any time.</p>
+              </div>
+              
+              
+              {/* Q and A */}
+              <div className="q__and__a">
+                <h3>What is the referral program?</h3>
+                <p>Every customer has a UNIQUE link bound to their account. Every successful referral used with your link will reward both you and your referral $10!</p>
+              </div>
+            </div>
+      ) : (
+       <div className="faq__questions__container">
+              <div className="faq__questions__container">
         {/* Q and A */}
         <div className="q__and__a">
           <h3>What if I don't like what I received?</h3>
           <p>At SeductiveBox, our customers have ZERO RISK on their subscriptions! You don't like what you received? Return it!</p>
+        </div>
+        
+        {/* Q and A */}
+        <div className="q__and__a">
+          <h3>How many days does shipping take?</h3>
+          <p>All monthly subscriptions with Seductivebox will be shipped to your doorstep within 3-5 business days.</p>
         </div>
         
         {/* Q and A */}
@@ -31,6 +72,17 @@ function FAQ() {
           <p>Your SeductiveBox varies on the package you select, but can be as low as 29.99! Of course, you will always have the option to skip a month’s delivery, and you can cancel at any time.</p>
         </div>
         
+        {/* Q and A */}
+        <div className="q__and__a">
+          <h3>How often is my box shipped?</h3>
+          <p>Every Subscription is shipped monthly. With our hassle free cancel, pause, skip, allows you to have a fully flexible subscription. Youre in full control!</p>
+        </div>
+        
+        {/* Q and A */}
+        <div className="q__and__a">
+          <h3>What if I have an issue with my order?</h3>
+          <p>All Issues with order should be directed to Support@Seductivebox.com and we will be more than happy to assist you!</p>
+        </div>
         
         {/* Q and A */}
         <div className="q__and__a">
@@ -38,6 +90,8 @@ function FAQ() {
           <p>Every customer has a UNIQUE link bound to their account. Every successful referral used with your link will reward both you and your referral $10!</p>
         </div>
       </div>
+       </div>
+      )}
       </div>
     </div>
   )
