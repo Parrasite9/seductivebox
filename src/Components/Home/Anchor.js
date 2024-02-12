cf06fdff35e8f265952594fbdbecd90a2fa60db6
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-11 18:50:17 -0600

    Tablet anchor has been completed 750px

--- src/Components/Home/Anchor.js
+++ src/Components/Home/Anchor.js
@@ -6,13 +6,15 @@ import WindowWidth from '../CustomHook/WindowWidth'
 function Anchor() {
 
   const width = WindowWidth()
-  const isMobile = width < 1000
+  const isMobile = width < 750
+  const isTablet = width >= 750 && width < 1000
+  const isDesktop = width > 1000
 
   return (
     <div className='Anchor'>
       <div className="anchor__container">
 
-        {isMobile ? (
+        {isMobile && (
           <div className="mobile__anchor">
             <div className="cta">
               <h2>Discover Something Sexy In You!</h2>
@@ -43,8 +45,42 @@ function Anchor() {
               <p>-Wendy S.</p>
             </div>
           </div>
-        ) : (
-          // Non-mobile content here
+        )}  
+        
+        {isTablet && (
+          <div className="tablet__container">
+            <div className="quote__container">
+              <h2>"Lingerie is not about seducing men; It's about embracing womanhood"</h2>
+              <p>-Dita Von Teese</p>
+            </div>
+
+            <div className="tablet__img__container">
+            <img src="images/anchor/model1.png" alt="model" />
+            </div>
+
+            <div className="tablet__img__container">
+            <img src="images/anchor/model2.png" alt="model" />
+            </div>
+
+            <div className="tablet__cta">
+              <h2>Discover Something Sexy In You!</h2>
+              <button>Take Style Quiz</button>
+            </div>
+
+            <div className="quote__container">
+              <h2>"My wife and I absolute LOVE our SeductiveBox subscription! This bring more excitement to our love life. Plus this is the only subscription that gets unwrapped TWICE!"</h2>
+              <p>-Wendy S.</p>
+            </div>
+
+            <div className="tablet__img__container">
+            
+            <img src="images/anchor/model3.png" alt="model" />
+
+            </div>
+          </div>
+        )}
+
+        {isDesktop && (
           <></>
         )}
       </div>
