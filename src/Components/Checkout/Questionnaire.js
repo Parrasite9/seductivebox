b32a25bf4f664793cdb8b343e6d61eaa97a4dcf5
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-22 17:56:44 -0600

    Question and questionnaire has been setup for functionality. Testing has not happened yet

--- src/Components/Checkout/Questionnaire.js
+++ src/Components/Checkout/Questionnaire.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 import '../../CSS/Checkout/Questionnaire.css'
+import Question from './Question'
 
 function Questionnaire() {
+  
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  const [selectedOptions, setSelectedOptions] = useState({})
+
+  const handleSelectedOption = (option) => {
+    const question = questions[currentQuestionIndex]
+    setSelectedOptions({...selectedOptions, [question.id]: option })
+
+    // MOVE TO THE NEXT QUESTION 
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1)
+    } else {
+      // ALL QUESTIONS ANSWERED, PROCEED TO CHECKOUT 
+    }
+  }
+
   return (
     <div className='Questionnaire'>
-      
+      {questions.length > 0 && (
+        <Question
+            question={question[currentQuestionIndex]}
+            onSelectOption={handleSelectedOption} />
+      )}
     </div>
   )
 }
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,9 +2,12 @@ import React from 'react'
 import '../../CSS/Dashboard/Dashboard.css'
 
 function Dashboard() {
+    
   return (
     <div className='Dashboard'>
-      
+      <div className="dashboard__container">
+
+      </div>
     </div>
   )
 }
