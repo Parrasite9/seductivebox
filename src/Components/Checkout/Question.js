076b41276b9c02633a67b8cb2afff3d94115f198
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-22 19:41:00 -0600

    Grid layout for media queries has been added

--- src/Components/Checkout/Question.js
+++ src/Components/Checkout/Question.js
@@ -1,24 +1,23 @@
 import React from 'react';
+import '../../CSS/Checkout/Question.css'
 
 function Question({ question, onSelectOption }) {
     return (
         <div className="Question">
+            <div className="question__title">
+                <h2>{question.title}</h2>
+            </div>
             <div className="questions__container">
-                <div className="question__title">
-                    <h2>{question.title}</h2>
-                </div>
-
-                <div className="question__details">
-                    {question.options.map((option, index) => (
-                        <button key={index} onClick={() => onSelectOption(option.text)}>
-                            <img src={option.photo} alt={option.text} />
-                            {option.text}
-                        </button>
-                    ))}
-                </div>
+                {question.options.map((option, index) => (
+                    <button key={index} onClick={() => onSelectOption(option.text)}>
+                        <img src={option.photo} alt={option.text} />
+                        {option.text}
+                    </button>
+                ))}
             </div>
         </div>
     );
 }
+
 
 export default Question;
