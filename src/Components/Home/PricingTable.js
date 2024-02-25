410adbc2fc44c48ac3a458117289df8a19659638
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-25 14:21:46 -0600

    redid the pricing table and added more CSS. Link to the style quiz is incorporated

--- src/Components/Home/PricingTable.js
+++ src/Components/Home/PricingTable.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../CSS/Home/PricingTable.css'
 
 const PricingTable = () => {
+  const plans = [
+    {
+      name: 'Disobedient',
+      price: '$29.99/mo',
+      features: ['Feature 1', 'Feature 2', 'Feature 3'],
+    },
+    {
+      name: 'SeductiveBox',
+      price: '$49.99/mo',
+      features: ['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4'],
+    },
+    {
+      name: 'Rebellious',
+      price: '$79.99/mo',
+      features: ['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4', 'Feature 5'],
+    },
+  ];
+
   return (
-    <div className='PricingTable'>
-      <div className="elfsight-app-c2eb41ba-bba0-400e-8c1e-63c64a0fc879" data-elfsight-app-lazy></div>
-    </div>
+    <>
+        <div className="pricing__header">
+            <h2>Pricing</h2>
+        </div>
+
+        <div className="pricing-table">
+        {plans.map((plan) => (
+            <div key={plan.name} className="plan">
+            <h3>{plan.name}</h3>
+            <p className="price">{plan.price}</p>
+            <ul>
+                {plan.features.map((feature) => (
+                <li key={feature}>{feature}</li>
+                ))}
+            </ul>
+            <Link to='/style-quiz'>
+                <button>Take The Style Quiz</button>
+            </Link>
+            </div>
+        ))}
+        </div>
+    </>
   );
 };
 
